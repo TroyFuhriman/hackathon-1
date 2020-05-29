dc1e610074eaa4ef8b1693b727895ba74654f251
Justin Mattson <justin.mattson@outlook.com>
2020-05-29 16:04:04 -0600

    add comment to front end.

--- front-end/app/Models/Comment.js
+++ front-end/app/Models/Comment.js
@@ -1,19 +1,40 @@
 export default class Comment {
   constructor(data) {
-    this.description = data.description
-    this.id = data.id
-    this.author = data.author
-    this.created = data.createdAt
-    this.edited = data.updatedAt
-    this.upVote = data.upvote || 0
-    this.downVote = data.downvote || 0
+    this.description = data.description;
+    this.id = data.id;
+    this.author = data.author;
+    this.created = data.createdAt;
+    this.edited = data.updatedAt;
+    this.upvotes = data.upvotes || 0;
+    this.downvotes = data.downvotes || 0;
   }
 
   get Template() {
-    return /*html*/`
-    <div class="col-12 card">
-    <p>${this.description}</p>
-        <h4>${this.author}</h4>
-</div>`
+    return /*html*/ `
+          <!-- COMMENT TEMPLATE -->
+          <div class="row d-flex">
+          <div class="col-3">
+            <div>
+              <i
+                class="far fa-arrow-alt-circle-up text-success"
+                onclick="app.commentsController.upvote('${this.id}')"
+              ></i>
+              ${this.upvotes}
+            </div>
+            <div>
+              <i
+                class="far fa-arrow-alt-circle-down text-danger"
+                onclick="app.commentsController.downvote('${this.id}')"
+              ></i>
+              ${this.downvotes}
+            </div>
+          </div>
+          <div class="col-8">
+            <span>${this.description}</span>
+            <span>~${this.author}</span>
+          </div>
+        </div>
+        <!-- END CARD TEMPLATE -->
+    `;
   }
 }
