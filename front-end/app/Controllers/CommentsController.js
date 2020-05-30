440a965ea244821ac018e52f36ea6a05890249fb
StevenPackard <littlepackard@gmail.com>
2020-05-29 20:25:54 -0600

    fixed get comments by post. Added up/downvote fun.

--- front-end/app/Controllers/CommentsController.js
+++ front-end/app/Controllers/CommentsController.js
@@ -5,33 +5,9 @@ import commentsService from "../Services/CommentsService.js";
 //Private
 function _draw() {
   let comments = store.State.comments;
-  let posts = store.State.posts
   console.log(comments)
   let template = ""
-  comments.forEach(c => document.getElementById(c.postId).innerHTML = template +=
-    `<div class="row d-flex">
-      <div class="col-3">
-        <div>
-          <i
-            class="far fa-arrow-alt-circle-up text-success"
-            onclick="app.commentsController.upvote('${c.id}')"
-          ></i>
-      ${c.upvotes}
-        </div>
-        <div>
-          <i
-            class="far fa-arrow-alt-circle-down text-danger"
-            onclick="app.commentsController.downvote('${c.id}')"
-          ></i>
-      ${c.downvotes}
-        </div>
-      </div>
-      <div class="col-8">
-        <span>${c.description}</span>
-        <span>~${c.author}</span>
-      </div>
-    </div>`)
-
+  comments.forEach(c => document.getElementById(c.postId).innerHTML = template += c.Template)
 }
 
 //Public
